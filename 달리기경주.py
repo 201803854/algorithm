14eb0d945bbbde8671e23a9842ee47cca0488584
201803854 <100855734+201803854@users.noreply.github.com>
2023-06-01 22:42:54 +0900

    Create 달리기경주.py

---
+++ 달리기경주.py
@@ -0,0 +1,7 @@
+echo "# algorithm" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/201803854/algorithm.git
+git push -u origin main
