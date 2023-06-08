c1140ea66ac0a37591c08582f2694b7da4cdc2df
201803854 <100855734+201803854@users.noreply.github.com>
2023-06-08 15:57:32 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/155652#

--- 둘만의 암호.py
+++ 둘만의 암호.py
@@ -1,8 +1,24 @@
 def solution(s, skip, index):
     answer = ''
-    for i in s:
-        count = sum(1 for c in skip if ord(i) < ord(c) <= ord(i) + index)
+    skip_set = set(skip)  # skip 문자열을 집합(set)으로 변환하여 효율적인 탐색을 위해 활용
+    
+    for char in s:
+        count = 0
+        check = 0
+        replaced_char = char  # replaced_char 변수를 밖으로 이동
         
-        replaced_char = chr((ord(i) + index + count - 97) % 26 + 97)
+        while True:
+            replaced_char = chr((ord(char) +1 + count - 97) % 26 + 97)
+
+            if replaced_char not in skip_set:
+                count += 1
+            else:
+                count += 1
+                check += 1
+            
+            if count >= index + check:
+                break
+        replaced_char = chr((ord(char) + count - 97) % 26 + 97)
         answer += replaced_char
+        
     return answer
